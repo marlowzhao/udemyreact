6140f8e0f98f12d47ea7087ea4ba946ace38ff02
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-07-22 22:32:36 +0200

    signin

--- facerecognitionbrain/src/components/Logo/Logo.js
+++ facerecognitionbrain/src/components/Logo/Logo.js
@@ -9,7 +9,7 @@ const Logo = () => {
   return (
     <div className='ma4 mt0'>
       <Tilt className="Tilt br2 shadow-3" options={{ max : 25 }} style={{ height: 120, width: 120 }} >
-      <div className='Tilt-inner pa1'> <img style={{paddingTop: '1px'}} alt='logo' src ={brain}/> </div>
+      <div className='Tilt-inner pa3'> <img style={{paddingTop: '3px'}} alt='logo' src ={brain}/> </div>
        </Tilt>
     </div>
   );
